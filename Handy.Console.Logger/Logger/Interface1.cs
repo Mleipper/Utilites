d0332a0d6810a2f45546853bc66823cbc3482dd8
Michael Leipper <michael.leipper@Gmail.com>
2020-10-26 14:33:46 +0000

    Simple Ilogger added to utils

---
+++ Handy.Console.Logger/Logger/Interface1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Handy.Logger.Logger
+{
+    public interface ILogger
+    {
+        void Debug(object message);
+        void Info(object message);
+        void Warning(object message, Exception exception = null);
+        void Error(object message, Exception exception = null);
+        void Criticial(object message, Exception exception = null);
+    }
+}
---
+++ Handy.Console.Logger/Logger/Logger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Handy.Logger.Logger
+{
+    public class Logger : ILogger
+    {
+        public void Debug(object message)
+        {
+            WriteLine(message);
+        }
+
+        public void Info(object message)
+        {
+            WriteLine(message, ConsoleColor.White);
+        }
+
+        public void Warning(object message, Exception exception = null)
+        {
+            WriteLine(message, ConsoleColor.Yellow, exception);
+        }
+
+        public void Error(object message, Exception exception = null)
+        {
+            WriteLine(message, ConsoleColor.DarkRed, exception);
+        }
+
+        public void Criticial(object message, Exception exception = null)
+        {
+            WriteLine(message, ConsoleColor.Red, exception);
+        }
+
+        private void WriteLine(object message, ConsoleColor? color = null, Exception exception = null)
+        {
+            if (color.HasValue)
+            {
+                Console.ForegroundColor = color.Value;
+            }
+
+            if (exception != null)
+            {
+                message += $" : {exception.Message} : {exception.StackTrace}";
+            }
+
+            Console.WriteLine($"{DateTime.UtcNow} - {message}");
+
+            if (color.HasValue)
+            {
+                Console.ResetColor();
+            }
+        }
+    }
+}
